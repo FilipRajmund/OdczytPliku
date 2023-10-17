2cb255b09c41bfdc33f1f7eb280c22bc6b16d1bd
filip <filip1@gamial.pl>
2023-10-17 15:16:52 +0200

    StandardCharsets.UTF_8

--- src/main/java/pl/FilipRajmund/Main.java
+++ src/main/java/pl/FilipRajmund/Main.java
@@ -1,6 +1,7 @@
 package pl.FilipRajmund;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,11 +11,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.print("śćł!");
         Path path = Paths.get("src/main/resources/dysk2.txt");
-        List<String> strings = Files.readAllLines(path);
+
+        List<String> strings = Files.readAllLines(path, StandardCharsets.UTF_8);
 
         for (String string : strings) {
             System.out.println(string);
         }
+
 
 
 
